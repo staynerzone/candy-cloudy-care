674c2c36f76d9f95d5b1fdb6c05971ca2f43f675
ptFive <johannes.steiner@protonmail.com>
2019-02-25 18:52:56 +0000

    create/delete

--- tasks/spawn-instance/task.sh
+++ tasks/spawn-instance/task.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 # Spawn random vm in random project
 
-set -x
-
 IMAGE_ID=$(openstack image list -f json | jq '.[] | select( .Name | contains("cirros")).ID' -r)
 FLAVOR_ID=$(shuf -n 1 <(openstack flavor list -f json | jq '.[].ID' -r))
-SG_ID=$(shuf -n 1 <(openstack security group list -f json | jq '.[].ID' -r))
+#SG_ID=$(shuf -n 1 <(openstack security group list -f json | jq '.[].ID' -r))
 AZ_ID=$(shuf -n 1 <(openstack availability zone list --compute -f json | jq '.[]["Zone Name"] | select(. | contains("internal") | not)' -r))
 NETWORK_ID=$(shuf -n 1 <(openstack network list  -f json | jq '.[] | select(.Name | contains("provider")|not).ID' -r))
 
 
+echo "Creating  ..."
+TIME=$(/usr/bin/time -f'%e' \
 openstack server create\
   --image $IMAGE_ID\
   --flavor $FLAVOR_ID\
-  --security-group $SG_ID\
   --network $NETWORK_ID\
   --wait\
-  TEST_INSTANCE
+  TEST_INSTANCE)
+echo "...created! ($TIME)"
+
+echo "Deleting ..."
+ID=$(<(openstack server list --all-projects -f json | jq '.[] | select( .Name | contains("TEST_INSTANCE")).ID' -r))
+openstack server delete $ID
+echo "...deleted!"
