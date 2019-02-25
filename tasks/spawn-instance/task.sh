a9c79585579a1796d3f8f71bd124ec24ac3d93a2
ptFive <johannes.steiner@protonmail.com>
2019-02-25 18:38:04 +0000

    no timing problem...

--- tasks/spawn-instance/task.sh
+++ tasks/spawn-instance/task.sh
@@ -4,15 +4,10 @@
 set -x
 
 IMAGE_ID=$(openstack image list -f json | jq '.[] | select( .Name | contains("cirros")).ID' -r)
-sleep 2
 FLAVOR_ID=$(shuf -n 1 <(openstack flavor list -f json | jq '.[].ID' -r))
-sleep 2
 SG_ID=$(shuf -n 1 <(openstack security group list -f json | jq '.[].ID' -r))
-sleep 2
 AZ_ID=$(shuf -n 1 <(openstack availability zone list --compute -f json | jq '.[]["Zone Name"] | select(. | contains("internal") | not)' -r))
-sleep 2
-NETWORK_ID=(shuf -n 1 <(openstack network list  -f json | jq '.[] | select(.Name | contains("provider")|not).ID' -r))
-sleep 2
+NETWORK_ID=$(shuf -n 1 <(openstack network list  -f json | jq '.[] | select(.Name | contains("provider")|not).ID' -r))
 
 
 openstack server create\
