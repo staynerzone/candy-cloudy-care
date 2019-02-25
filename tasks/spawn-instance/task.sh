2b21b7e007ae85f5943ec0135d9a3b69ac0b66fa
ptFive <johannes.steiner@protonmail.com>
2019-02-25 18:31:59 +0000

    find network bug

--- tasks/spawn-instance/task.sh
+++ tasks/spawn-instance/task.sh
@@ -7,7 +7,9 @@ IMAGE_ID=$(openstack image list -f json | jq '.[] | select( .Name | contains("ci
 FLAVOR_ID=$(shuf -n 1 <(openstack flavor list -f json | jq '.[].ID' -r))
 SG_ID=$(shuf -n 1 <(openstack security group list -f json | jq '.[].ID' -r))
 AZ_ID=$(shuf -n 1 <(openstack availability zone list --compute -f json | jq '.[]["Zone Name"] | select(. | contains("internal") | not)' -r))
-NETWORK_ID=(shuf -n 1 <(openstack network list --internal -f json | jq '.[].ID' -r))
+NETWORK_ID=(shuf -n 1 <(openstack network list  -f json | jq '.[] | select(.Name | contains("provider")|not).ID' -r))
+
+
 
 openstack server create\
   --image $IMAGE_ID\
