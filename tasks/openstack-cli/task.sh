7a2f0409bef691c63ba0f0d22bfd931663600b1e
ptFive <johannes.steiner@protonmail.com>
2019-02-25 17:21:54 +0000

    first approach with time

--- tasks/openstack-cli/task.sh
+++ tasks/openstack-cli/task.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Set the colors
+
 RED='\033[0;31m' # for state
 GREEN='\033[0;32m' # for state
 CYAN='\033[0;36m' # for internal exclusions
@@ -7,4 +8,10 @@ PURPLE='\033[0;35m' # for service names
 ORANGE='\033[0;33m' # for url names
 NC='\033[0m'
 
-openstack catalog list
+SLOW_APIS=0
+
+KEYSTONE_T=5.0
+
+KEYSTONE_TIME=$(/usr/bin/time -f'%e' openstack token issue 2>&1 1>/dev/null)
+if [[ $KEYSTONE_TIME > 0 && $KEYSTONE_TIME > $KEYSTONE_T ]]; then SLOW_APIS=`expr $SLOW_APIS + 1` ; else echo "keystone: i.o ($KEYSTONE_TIME)" ; fi
+
